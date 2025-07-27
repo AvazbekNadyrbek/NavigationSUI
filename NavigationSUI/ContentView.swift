4225176e9a7249f743960b3a2ed9107495c6cb99
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-07-27 15:32:03 +0200

    Here we are learned how we can use NavigationStack and how we can send a data between a Views

--- NavigationSUI/ContentView.swift
+++ NavigationSUI/ContentView.swift
@@ -7,18 +7,95 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @Binding var path: [Page]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Divider()
+            
+            ForEach(0...10, id: \.self) { page in
+                //NavigationLink("to page -> \(page)", value: page)
+                Button {
+                    path.append(.Second(page))
+                } label: {
+                    Text("to page -> \(page)")
+                }
+                
+            }
+            
+            Divider()
+            
+            Button{
+                path.append(.Profile("Avazbek"))
+            } label: {
+                Text("Go to profile page")
+            }
+            
+            Button {
+                path.append(.Setting)
+            } label: {
+                Text("Go to setting page")
+                    .foregroundStyle(.red)
+            }
+            
+            
         }
         .padding()
+        
     }
 }
 
-#Preview {
-    ContentView()
+
+//#Preview {
+//    ContentView(path: Setting)
+//}
+
+struct SecondPage: View {
+    
+    var pageNumber: Int = 0
+    @State var text: String = ""
+    @Binding var path: [Page]
+    
+    init(pageNumber: Int, path: Binding<[Page]>) {
+        self.pageNumber = pageNumber
+        self._path = path
+        print(pageNumber)
+    }
+    
+    var body: some View {
+        VStack{
+            Text("This is page: \(pageNumber)")
+            
+            Button {
+                path.append(.Setting)
+            } label: {
+                Text("Back")
+            }
+
+        }
+    }
+}
+
+struct ProfilePage: View {
+    
+    var name: String
+    
+    var body: some View {
+        Text("Profile Page, my name is \(name)")
+    }
+}
+
+struct SettingsPage: View {
+    var body: some View {
+        Text("Settings Page, path number is 11")
+    }
 }
--- NavigationSUI/NavigationSUIApp.swift
+++ NavigationSUI/NavigationSUIApp.swift
@@ -7,11 +7,36 @@
 
 import SwiftUI
 
+enum Page: Hashable {
+    case Second(Int)
+    case Profile(String)
+    case Setting
+}
+
 @main
 struct NavigationSUIApp: App {
+    
+    @State var path: [Page] = []
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack(path: $path) {
+                ContentView(path: $path)
+                    .navigationDestination(for: Page.self) { page in
+                        // page its path (url)
+                        // and passing the argument
+                        
+                        switch page {
+                        case .Profile(let nameProfile):
+                            ProfilePage(name: nameProfile)
+                        case .Second(let pageNumber):
+                            SecondPage(pageNumber: pageNumber, path: $path)
+                        case .Setting:
+                            SettingsPage()
+                        }
+                    }
+            }
+            
         }
     }
 }
